47ec0ca36425f611bbbc1a6a7f901f70ec0273b9
Saša Drmić <sasadrmic032@gmail.com>
2017-07-14 17:46:29 +0200

    Added homepage animations

--- js/main.js
+++ js/main.js
@@ -16,3 +16,47 @@ $(window).scroll(function() {
 		$hambg.removeClass('hambgScrolled');
 	}
 });
+
+// Animations
+
+function animateItems() {
+    $('.what-i-do__item').css('opacity','0').viewportChecker({
+    classToAdd: 'animated fadeIn',
+  });
+}
+
+function animateAbout() {
+    $('.who-am-i p').css('opacity','0').viewportChecker({
+    classToAdd: 'animated fadeIn',
+    offset: 150
+  });
+}
+
+function animateSigniture() {
+    $('.who-am-i .signiture').css('opacity','0').viewportChecker({
+    classToAdd: 'animated fadeIn'
+  });
+}
+
+function animateMyValues() {
+    $('.my-values li').css('opacity','0').viewportChecker({
+    classToAdd: 'animated fadeIn'
+  });
+}
+
+function animateFooterQuote() {
+    $('.quote--footer').css('opacity','0').viewportChecker({
+    classToAdd: 'animated fadeIn'
+  });
+}
+
+function animateAll(){
+	animateItems();
+	animateAbout();
+	animateMyValues()
+	animateSigniture()
+	animateFooterQuote()
+}
+
+animateAll();
+
