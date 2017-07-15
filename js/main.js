2d57f2d2d4a4df45fb48c0c75f21f00acdcf2cab
Saša Drmić <sasadrmic032@gmail.com>
2017-07-15 11:23:27 +0200

    Added scrolling animation

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var $navbarToggler = $('.navbar-toggler');
 var $hambg = $('.hamburger-menu');
 
 $(window).scroll(function() {
-	if($(this).scrollTop() > $headerHight){
+	if($(this).scrollTop() > $headerHight - 100){
 		$nav.addClass('scrolledNav');
 		$navbarToggler.addClass('togglerScrolled');
 		$hambg.addClass('hambgScrolled');
@@ -16,6 +16,43 @@ $(window).scroll(function() {
 		$hambg.removeClass('hambgScrolled');
 	}
 });
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 // Animations
 
